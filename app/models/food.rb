2a61f778e91cca92c58744d9cbd449bd45d67b13
moco2-t <moco2.t@gmail.com>
2021-05-28 10:33:13 +0900

    Change of the part requested for change #1

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,6 @@
 class Food < ApplicationRecord
     has_ancestry
     has_many :ingredinets
-    validates :name,:ancestry, {presence:true}
+    validates :name,:ancestry, {presence:true}　#空欄
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "index" => "home#index" #ログイン前index
-  #user
+  #ログイン、アカウント
   get "users/login_form" => "user#login_form" #ログイン画面
   post "users/login" => "user#login" #ログインform
   post "logout" => "user#logout" #ログアウト
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   delete "/users/delete" => "user#destroy" #アカウント削除
   get "users/:id/edit" => "user#edit" #アカウント編集
   post "users/update" => "user#update" #アカウント編集form
-
+  #レシピ機能
   get "recipes/index" => "recipe#index" #レシピ一覧
   get "recipes/new" => "recipe#new" #レシピ登録ページ
   post "recipes/create" => "recipe#create" #レシピ登録
