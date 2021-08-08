254425bcb02762c993bb70a2c3e437d856ac2527
moco2-t <moco2.t@gmail.com>
2021-08-08 14:25:32 +0900

    Add meal table to DB # 19

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_08_043053) do
+ActiveRecord::Schema.define(version: 2021_08_08_052152) do
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 2021_08_08_043053) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "amount"
+  end
+
+  create_table "meals", force: :cascade do |t|
+    t.date "start_time"
+    t.integer "recipe_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "recipes", force: :cascade do |t|
---
+++ spec/factories/meals.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :meal do
+    start_time { "2021-08-08" }
+    recipe_id { 1 }
+  end
+end
