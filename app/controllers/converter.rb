0e7515f3b8ab984a35d12099c0222bb1e1616e2f
TOMOMI MIURA <tomomimiura@TOMOMInoMacBook-Air.local>
2022-02-03 22:33:41 +0900

    Change @quantity and conditional branch in converter.rb file #33

--- app/controllers/converter.rb
+++ app/controllers/converter.rb
@@ -4,9 +4,9 @@ module Converter
         @gen_amount = @amount.tr('〇一二三四五六七八九', '0123456789').tr('０-９ａ-ｚＡ-Ｚ／〜', '0-9a-zA-Z/~').delete("^0-9/~","^０-９／〜")
         if @temporary_food_id === food_hash_unit["none"] # unitがnil=調味料,牛乳
             quantity_conversion_of_seasonings
-        elsif @temporary_food_id === food_hash["キャベツ"]&&@amount.include?("枚")
+        elsif @temporary_food_id === food_hash["キャベツ"]
             quantity_conversion_cabbage
-        elsif @temporary_food_id === food_hash["鶏むね肉"]&&@amount.include?("枚")||food_hash["鶏もも肉"]&&@amount.include?("枚")
+        elsif @temporary_food_id === food_hash["鶏むね肉"]||food_hash["鶏もも肉"]
             quantity_conversion_chicken_breast
         else
             quantity_conversion
@@ -49,22 +49,20 @@ module Converter
 
     def quantity_conversion
         criterion_value = Food.find_by(id:@temporary_food_id).criterion.to_f
-        if @amount.include?("g")||@amount.include?("グラム")
+        if @amount.include?("g")||@amount.include?("ｇ")||@amount.include?("グラム")
             if @amount.include?("~")||@amount.include?("〜") 
-                @quantity = Rational(@gen_amount.sub!(/~.*/m,"")).to_f/criterion_value        
+                @quantity = (Rational(@gen_amount.sub!(/~.*/m,""))/criterion_value.to_f).floor(2)      
             else
-                @quantity = Rational(@gen_amount).to_f/criterion_value
-            end
-        elsif @amount.include?("本")||@amount.include?("合")||@amount.include?("玉")||@amount.include?("尾")||@amount.include?("個")||@amount.include?("束")||@amount.include?("袋")||@amount.include?("片")||@amount.include?("株")||@amount.include?("パック")||@amount.include?("切れ")||@amount.include?("枚")||@amount.include?("切")
-            if @amount.include?("~")||@amount.include?("〜")
-                @quantity = Rational(@gen_amount.sub!(/~.*/m,"")).to_f
-            else
-                @quantity = Rational(@gen_amount).to_f
+                @quantity = (Rational(@gen_amount).to_f/criterion_value.to_f).floor(2)
             end
         elsif @amount.include?("適量")||@amount.include?("少々")||@amount.include?("ひとつまみ")
             @quantity = nil
         else
-            @quantity = nil
+            if @amount.include?("~")||@amount.include?("〜") 
+                @quantity = (Rational(@gen_amount.sub!(/~.*/m,"")).to_f).floor(2)      
+            else
+                @quantity = (Rational(@gen_amount).to_f).floor(2)
+            end
         end
     end
 
@@ -73,20 +71,45 @@ module Converter
     end
 
     def quantity_conversion_cabbage
-        if @amount.include?("~")||@amount.include?("〜")
-            @quantity = Rational(@gen_amount.sub!(/~.*/m,"")).to_f*23/1200
-        else
-            @quantity = Rational(@gen_amount).to_f*23/1200
+        criterion_value = Food.find_by(id:@temporary_food_id).criterion.to_f
+        if @amount.include?("枚")
+            if @amount.include?("~")||@amount.include?("〜")
+                @quantity = (Rational(@gen_amount.sub!(/~.*/m,""))*23/1200.to_f).floor(2)
+            else
+                @quantity = (Rational(@gen_amount)*23/1200.to_f).floor(2)
+            end
+        elsif @amount.include?("g")||@amount.include?("ｇ")||@amount.include?("グラム")
+            if @amount.include?("~")||@amount.include?("〜")
+                @quantity = (Rational(@gen_amount.sub!(/~.*/m,""))/criterion_value.to_f).floor(2)
+            else
+                @quantity = (Rational(@gen_amount)/criterion_value.to_f).floor(2)
+            end
+        else 
+            if @amount.include?("~")||@amount.include?("〜")
+                @quantity = (Rational(@gen_amount.sub!(/~.*/m,"")).to_f).floor(2)
+            else
+                @quantity = (Rational(@gen_amount).to_f).floor(2)
+            end
         end
     end
 
     def quantity_conversion_chicken_breast
-        if @amount.include?("~")||@amount.include?("〜")
-            @quantity = Rational(@gen_amount.sub!(/~.*/m,"")).to_f*250
-        else
-            @quantity = Rational(@gen_amount).to_f*250
+        criterion_value = Food.find_by(id:@temporary_food_id).criterion.to_f
+        if @amount.include?("枚")
+            if @amount.include?("~")||@amount.include?("〜")
+                @quantity = (Rational(@gen_amount.sub!(/~.*/m,"")).to_f*250.to_f).floor(2)
+            else
+                @quantity = (Rational(@gen_amount).to_f*250.to_f).floor(2)
+            end
+        elsif @amount.include?("g")||@amount.include?("ｇ")||@amount.include?("グラム")
+            if @amount.include?("~")||@amount.include?("〜")
+                @quantity = (Rational(@gen_amount.sub!(/~.*/m,""))/criterion_value.to_f).floor(2)
+            else
+                @quantity = (Rational(@gen_amount)/criterion_value.to_f).floor(2)
+            end
+        else 
+            @quantity = nil
         end
     end
 
 end
-
