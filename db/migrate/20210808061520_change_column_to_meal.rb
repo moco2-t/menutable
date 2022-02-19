1fc00efc7f95bb99ce198309caad3e51b8935dda
TOMOMI MIURA <moco2.t@gmail.com>
2022-02-19 22:38:34 +0900

    Modifying the migration file for the meals model #52

--- db/migrate/20210808061520_change_column_to_meal.rb
+++ db/migrate/20210808061520_change_column_to_meal.rb
@@ -1,7 +1,7 @@
-class ChangeColumnTomenu < ActiveRecord::Migration[6.1]
+class ChangeColumnTomeal < ActiveRecord::Migration[6.1]
   def change
-    remove_column :menus, :start_time
-    add_column :menus, :start_date, :date
+    remove_column :meals, :start_time
+    add_column :meals, :start_date, :date
     #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
     #Ex:- change_column("admin_users", "email", :string, :limit =>25)
   end
